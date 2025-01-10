197bb5ae2178ea2bfd5de3452fe5327c66cb8313
Diego Minu Vargas <minuprime12@gmail.com>
2025-01-10 12:12:53 -0500

    Creando el menu de opciones y los metodos para cada opcion en major

--- src/main/java/com/challenge/literatura/Major/Major.java
+++ src/main/java/com/challenge/literatura/Major/Major.java
@@ -17,5 +17,59 @@ public class Major {
         System.out.println(json);
         var datos = convierteDatos.obtenerDatos(json, Datos.class);
         System.out.println(datos);
+
+        var opcion = -1;
+        while(opcion != 0) {
+            var menu = """
+                        1- Buscar libro por titulo
+                        2- Listar libros registrados
+                        3- Listar autores registrados
+                        4- Listar autores vivos en un determinado año
+                        5- Listar libros por idioma
+                        0- Salir
+                       """;
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            
+            switch (opcion) {
+                case 1:
+                    buscarLibroPorTitulo();
+                    break;
+                case 2:
+                    listarLibrosRegistrados();
+                    break;
+                case 3:
+                    listarAutoresRegistrados();
+                    break;
+                case 4:
+                    listarAutoresVivosPorAno();
+                    break;
+                case 5:
+                    listarLibrosPorIdioma();
+                    break;
+                case 0:
+                    System.out.println("Gracias por usar esta aplicacion.... Saliendo");
+                    break;
+                default:
+                    System.out.println("Opcion invalida");
+            }
+            
+        }
+    }
+
+    private void buscarLibroPorTitulo() {
+    }
+
+    private void listarLibrosRegistrados() {
+    }
+
+    private void listarAutoresRegistrados() {
+    }
+
+    private void listarAutoresVivosPorAno() {
+    }
+
+    private void listarLibrosPorIdioma() {
     }
 }
